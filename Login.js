2f34de05c6005bde3c37ce46d381d5d22e7a20b5
AMAN GUPTA <113255969+Amangupta97656@users.noreply.github.com>
2023-10-24 20:09:43 +0530

    Create Login.js

---
+++ Login.js
@@ -0,0 +1,50 @@
+// src/components/Login.js
+
+import React, { useState } from 'react';
+import { useHistory } from 'react-router-dom';
+
+const Login = () => {
+  const [formData, setFormData] = useState({ username: '', password: '' });
+  const history = useHistory();
+
+  const handleInputChange = (e) => {
+    const { name, value } = e.target;
+    setFormData({ ...formData, [name]: value });
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    // Implement your authentication logic here
+    // For this example, we'll just redirect to the welcome page
+    history.push('/welcome');
+  };
+
+  return (
+    <div>
+      <h2>Login</h2>
+      <form onSubmit={handleSubmit}>
+        <div>
+          <label>Username:</label>
+          <input
+            type="text"
+            name="username"
+            value={formData.username}
+            onChange={handleInputChange}
+          />
+        </div>
+        <div>
+          <label>Password:</label>
+          <input
+            type="password"
+            name="password"
+            value={formData.password}
+            onChange={handleInputChange}
+          />
+        </div>
+        <button type="submit">Login</button>
+      </form>
+    </div>
+  );
+};
+
+export default Login;
